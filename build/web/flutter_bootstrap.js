5350c718cf1ece7b6973d1255f4531023b69b890
Eswar <eswarmahendraj@gmail.com>
2026-08-03 16:32:01 +0530

    fix: ORBIT v16 - remove incompatible record pkg, add WRITE_EXTERNAL_STORAGE
    - Removed record: ^5.0.0 (record_linux 0.7.2 incompatible with record_platform_interface 1.6.0)
    - orbit_dm_screen.dart is in lib/screens/dm/ (dead code until navigation wired)
    - orbit_dm_service.dart confirmed at lib/services/ (19KB)
    - WRITE_EXTERNAL_STORAGE added to AndroidManifest.xml
    - APK: ORBIT_v16_fixed.apk (70MB)

--- build/web/flutter_bootstrap.js
+++ build/web/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"a10d8ac38de835021c8d2f920dbf50a920ccc0
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "252983795" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "1180691413" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
